e4ccc78ef98e34712ccec7f8618b1afce27e53c9
itaintkevin <kpjtofficial@gmail.com>
2022-10-08 15:33:15 +0530

    Added React Router dependency

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { Routes, Route, Link } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-
+    <div className="flex justify-center items-center h-screen w-screen">
+      <h1 className="text-center text-7xl">Hello React</h1>
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes> */}
     </div>
   );
 }
